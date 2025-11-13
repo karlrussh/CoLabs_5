3e949604dc274a00bc60ebd33c6bd8ec041618c0
Cory Tyson <coryzack@gmail.com>
2025-11-13 19:19:46 +0000

    Phone System added

---
+++ Assets/_Scripts/PhoneScript.cs
@@ -0,0 +1,67 @@
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PhoneScript : MonoBehaviour
+{
+    [SerializeField] private GameObject Phone;
+    [SerializeField] private GameObject startPos, endPos;
+    private bool moveUp, MoveDown;
+    [SerializeField] private GameObject ImgTop, ImgBottom;
+    [SerializeField] private TextMeshProUGUI CallerID;
+    [SerializeField] private AudioSource PhoneAudio;
+    private AudioClip phoneAudioClip;
+    [SerializeField] private Animator hinge;
+
+    private float startTime, journeyLength;
+    private float speed = 200;
+    public void PhoneCall(PhoneSO phoneData)
+    {
+        MoveDown = false;
+        moveUp = true;
+        startTime = Time.time;
+        journeyLength = Vector3.Distance(startPos.transform.position, endPos.transform.position);
+        ImgTop.GetComponent<Image>().sprite = phoneData.callerPfpTop;
+        ImgBottom.GetComponent<Image>().sprite = phoneData.callerPfpBottom;
+        CallerID.text = phoneData.callerID;
+        PhoneAudio.clip = phoneData.ringTone;
+        PhoneAudio.Play();
+        phoneAudioClip = phoneData.phoneCall;
+    }
+
+    public void AcceptCall()
+    {
+        PhoneAudio.clip = phoneAudioClip;
+        PhoneAudio.Play();
+        hinge.SetBool("Bobbing", true);
+    }
+
+
+
+
+    public void EndPhoneCall()
+    {
+        moveUp = false;
+        MoveDown = true;
+        startTime = Time.time;
+        PhoneAudio.Stop();
+        hinge.SetBool("Bobbing", false);
+    }
+
+    private void Update()
+    {
+        if(moveUp)
+        {
+            float distCovered = (Time.time - startTime) * speed;
+            float fractionOfJourney = distCovered / journeyLength;
+            Phone.transform.position = Vector3.Lerp(startPos.transform.position, endPos.transform.position, fractionOfJourney);
+        }
+        if (MoveDown)
+        {
+            float distCovered = (Time.time - startTime) * speed;
+            float fractionOfJourney = distCovered / journeyLength;
+            Phone.transform.position = Vector3.Lerp(endPos.transform.position, startPos.transform.position, fractionOfJourney);
+        }
+    }
+}
