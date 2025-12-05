94691851ec0690a76e2966e9480a6e4cbbaab9ad
AWinn2004 <108543292+AWinn2004@users.noreply.github.com>
2025-12-05 04:25:20 +0000

    Feature: Added interface to destructible objects

--- Assets/_Scripts/Destructable object/BaseDestructObject.cs
+++ Assets/_Scripts/Destructable object/BaseDestructObject.cs
@@ -1,13 +1,17 @@
 using UnityEngine;
 
-public class BaseDestructObject : MonoBehaviour
+public class BaseDestructObject : MonoBehaviour, IShootable
 {
     [SerializeField] private int hitPoints = 1;
     [SerializeField] private GameObject pickup;
+
     public virtual void DropPickup()
     {
+        if (pickup = null) return;
         Debug.Log("Dropping: " +  pickup);
-        Instantiate(pickup);
+        GameObject pickupInstance = Instantiate(pickup/*, this.transform,true*/);
+        pickupInstance.transform.position = this.transform.position;
+
     }
 
     private void DestroyObject()
@@ -18,6 +22,7 @@ public class BaseDestructObject : MonoBehaviour
 
     public void HandleOnBulletHit()
     {
+        Debug.Log("Worked!");
         if (hitPoints >= 0)
         {
             DestroyObject();
@@ -25,5 +30,4 @@ public class BaseDestructObject : MonoBehaviour
         else hitPoints -= 1;
 
     }
-
 }
